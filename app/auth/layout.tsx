94432536ee5db6f1bfe000960561188a73f1bc1b
Apple <apple@Apples-MacBook-Pro.local>
2024-10-12 23:32:07 +0100

    fixed breaking ui changes and link to login

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <section className="bg-custom-radial w-full h-[100vh] text-primary-400 bg-[length:16px_16px]">
       <main className="grid place-items-center">
-        <div className="bg-secondary-400 py-7 px-9 flex flex-col justify-center items-center w-[450px]  border my-3 border-1 border-grey-200 rounded">
+        <div className="bg-secondary-400 py-7 px-9 flex flex-col justify-center items-center  box-border  w-full vsm:w-[450px]  border my-3 border-1 border-grey-200 rounded">
           <Link href="/">
             <Image
               src="/lt2.svg"
